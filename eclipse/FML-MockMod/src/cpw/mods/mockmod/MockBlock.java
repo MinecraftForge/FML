2012e41091e5997f3c483f9f8b53c5a8939b54ed
JP Sugarbroad <taral@taral.net>
2012-08-18 15:34:42 -0700

    Put the mock block in creative inventory so it can be seen. Add block id debug.

--- eclipse/FML-MockMod/src/cpw/mods/mockmod/MockBlock.java
+++ eclipse/FML-MockMod/src/cpw/mods/mockmod/MockBlock.java
@@ -1,12 +1,15 @@
 package cpw.mods.mockmod;
 
 import net.minecraft.src.Block;
+import net.minecraft.src.CreativeTabs;
 import net.minecraft.src.Material;
 
 public class MockBlock extends Block
 {
     public MockBlock(int id)
     {
-        super(id,Material.field_76259_v);
+        super(id, 138, Material.field_76259_v);
+        func_71849_a(CreativeTabs.field_78026_f);
+        System.out.printf("Mockblock is id %d\n", id);
     }
 }
