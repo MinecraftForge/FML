13e23bb1ea3b5a89076fd28f3826845d19d32590
Abrar Syed <sacabrarsyed@gmail.com>
2012-11-11 14:31:24 -0600

    add player arg NetworkRegistry

--- common/cpw/mods/fml/common/network/NetworkRegistry.java
+++ common/cpw/mods/fml/common/network/NetworkRegistry.java
@@ -210,7 +210,7 @@ public class NetworkRegistry
     {
         for (IConnectionHandler handler : connectionHandlers)
         {
-            handler.connectionClosed(manager);
+            handler.connectionClosed(manager, player);
         }
         activeChannels.removeAll(player);
     }
