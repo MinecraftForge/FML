c48957a7219d6fd2d638b8af80c4090ac8a570e2
AbrarSyed <sacabrarsyed@gmail.com>
2015-05-02 16:21:18 -0500

    FML version properties file is no longer required at dev time

--- src/main/java/net/minecraftforge/fml/common/Loader.java
+++ src/main/java/net/minecraftforge/fml/common/Loader.java
@@ -54,6 +54,7 @@ import com.google.common.base.CharMatcher;
 import com.google.common.base.Function;
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
@@ -180,7 +181,7 @@ public class Loader
     private Loader()
     {
         modClassLoader = new ModClassLoader(getClass().getClassLoader());
-        if (!mccversion.equals(MC_VERSION))
+        if (!mccversion.equals(MC_VERSION) && Strings.isNullOrEmpty(System.getProperty("net.minecraftforge.gradle.GradleStart.srg.srg-mcp")))
         {
             FMLLog.severe("This version of FML is built for Minecraft %s, we have detected Minecraft %s in your minecraft jar file", mccversion, MC_VERSION);
             throw new LoaderException(String.format("This version of FML is built for Minecraft %s, we have detected Minecraft %s in your minecraft jar file", mccversion, MC_VERSION));
